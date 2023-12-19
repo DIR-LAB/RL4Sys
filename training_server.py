69f95b1d3d587553ab66a8039d8b02335883d434
Dong Dai <daidongly@gmail.com>
2023-12-18 23:09:28 -0500

    the schedule example should be ready

--- training_server.py
+++ training_server.py
@@ -11,8 +11,8 @@ from models.kernel_ac import RLActorCritic
 from reply_buffer import ReplayBuffer
 import zmq
 import json
-from trajectory import RL4SysTrajectory  # Assuming Trajectory class is defined
-
+from trajectory import RL4SysTrajectory
+from utils.logger import EpochLogger, setup_logger_kwargs  # Assuming Trajectory class is defined
 
 class TrainingServer:
     def __init__(self, flatten_obs_dim, kernel_size, kernel_dim,
@@ -23,6 +23,14 @@ class TrainingServer:
                  train_v_iters=80, lam=0.97, max_ep_len=1000,
                  target_kl=0.01):
         
+        current_dir = os.getcwd()
+        log_data_dir = os.path.join(current_dir, './logs/')
+
+        logger_kwargs = setup_logger_kwargs(
+            "rl4sys-ppo-scheduler", seed=seed, data_dir=log_data_dir)
+        self.logger = EpochLogger(**logger_kwargs)
+        self.logger.save_config(locals())
+
         seed += 10000 * os.getpid()
         torch.manual_seed(seed)
         np.random.seed(seed)
@@ -46,6 +54,7 @@ class TrainingServer:
         self.pi_optimizer = Adam(self.model.pi.parameters(), lr=self.pi_lr)
         self.vf_optimizer = Adam(self.model.v.parameters(), lr=self.vf_lr)
 
+        self.logger.setup_pytorch_saver(self.model)
         # send the initial model
         self.send_model()
 
@@ -96,22 +105,44 @@ class TrainingServer:
         socket.bind("tcp://*:5555")
 
         traj = 0
+        epoch = 0
 
         while True:
             trajectory_data = socket.recv_string()
             traj += 1
             trajectory = self.from_json(trajectory_data)
+            ep_ret, ep_len = 0, 0
             
             for r4a in trajectory.actions:
                 # Process each RL4SysAction in the trajectory
+                ep_ret += r4a.rew
+                ep_len += 1
                 if not r4a.done:
                     self.replay_buffer.store(r4a.obs, r4a.act, r4a.mask, r4a.rew, r4a.val, r4a.logp)
+                    self.logger.store(VVals=r4a.val)
                 else:
                     self.replay_buffer.finish_path(r4a.rew)
-                
+                    self.logger.store(EpRet=ep_ret, EpLen=ep_len)
+            
             # get enough trajectories for training the model
-            if traj >= self.traj_per_epoch:
+            if traj >= self.traj_per_epoch:  
+                epoch += 1              
                 self.train_model()
+                # Log info about epoch
+                self.logger.log_tabular('Epoch', epoch)
+                self.logger.log_tabular('EpRet', with_min_and_max=True)
+                self.logger.log_tabular('EpLen', with_min_and_max=True)
+                self.logger.log_tabular('VVals', with_min_and_max=True)
+                self.logger.log_tabular('LossPi', average_only=True)
+                self.logger.log_tabular('LossV', average_only=True)
+                self.logger.log_tabular('DeltaLossPi', average_only=True)
+                self.logger.log_tabular('DeltaLossV', average_only=True)
+                self.logger.log_tabular('Entropy', average_only=True)
+                self.logger.log_tabular('KL', average_only=True)
+                self.logger.log_tabular('ClipFrac', average_only=True)
+                self.logger.log_tabular('StopIter', average_only=True)
+                self.logger.dump_tabular()
+
                 # after finish training, we need to reset things and sync the model
                 self.send_model()
 
@@ -144,10 +175,12 @@ class TrainingServer:
             loss_pi, pi_info = self.compute_loss_pi(data)
             kl = (pi_info['kl'])
             if kl > 1.5 * self.target_kl:
-                print('Early stopping at step %d due to reaching max kl.' % i)
+                self.logger.log('Early stopping at step %d due to reaching max kl.' % i)
                 break
             loss_pi.backward()
             self.pi_optimizer.step()
+        
+        self.logger.store(StopIter=i)
 
         # Value function learning
         for i in range(self.train_v_iters):
@@ -155,3 +188,10 @@ class TrainingServer:
             loss_v = self.compute_loss_v(data)
             loss_v.backward()
             self.vf_optimizer.step()
+
+        # Log changes from update
+        kl, ent, cf = pi_info['kl'], pi_info_old['ent'], pi_info['cf']
+        self.logger.store(LossPi=pi_l_old, LossV=v_l_old,
+                     KL=kl, Entropy=ent, ClipFrac=cf,
+                     DeltaLossPi=(loss_pi.item() - pi_l_old),
+                     DeltaLossV=(loss_v.item() - v_l_old))
